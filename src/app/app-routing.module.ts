6e1b2236e202d9f4c96289904eab578b6e81890d
JoniMeichtry11 <joni.meichtry@gmail.com>
2022-07-25 10:11:46 -0300

    feat: routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssignmentRecordPageComponent } from '@modules/assignment-record/pages/assignment-record-page/assignment-record-page.component';
+import { DeparturePageComponent } from '@modules/home/pages/departure-page/departure-page.component';
 // components
 import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
+import { TerritoryPageComponent } from '@modules/home/pages/territory-page/territory-page.component';
 
 const routes: Routes = [
-  {
-    path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
-  },
   {
     path: '',
     component: HomePageComponent,
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'territorios',
+    component: TerritoryPageComponent,
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'salidas',
+    component: DeparturePageComponent,
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'registro-territorios',
+    component: AssignmentRecordPageComponent,
+    loadChildren: () => import('./modules/assignment-record/assignment-record.module').then(m => m.AssignmentRecordModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   }
 ];
 
