3a48a841624329ee852c63547364b196cc956d21
JoniMeichtry11 <joni.meichtry@gmail.com>
2022-08-12 17:09:55 -0300

    feat: add check input for apples

--- src/app/modules/territory/pages/card-territory/card-territory.component.ts
+++ src/app/modules/territory/pages/card-territory/card-territory.component.ts
@@ -8,6 +8,28 @@ import { RouterBreadcrumMockService } from '@shared/mocks/router-breadcrum-mock.
 })
 export class CardTerritoryComponent implements OnInit {
   routerBreadcrum: any = [];
+  applesData: any = [
+    {
+      name: "Manzana 1",
+      checked: true
+    },
+    {
+      name: "Manzana 2",
+      checked: true
+    },
+    {
+      name: "Manzana 3",
+      checked: false
+    },
+    {
+      name: "Manzana 4",
+      checked: false
+    },
+    {
+      name: "Manzana 5",
+      checked: false
+    },
+  ];
   constructor(private routerBreadcrumMockService: RouterBreadcrumMockService) { 
     this.routerBreadcrum = routerBreadcrumMockService.getBreadcrum();
   }
