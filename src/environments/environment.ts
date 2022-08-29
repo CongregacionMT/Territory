9e4c4c1a8fe1d1be343ffe844bf5166699d65dae
JoniMeichtry11 <joni.meichtry@gmail.com>
2022-08-29 17:05:20 -0300

    feat: add Firebase in the App

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'territorios-422c2',
+    appId: '1:1086542552314:web:a5afc53afaa2f305ebfd43',
+    storageBucket: 'territorios-422c2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBI1BYUtYjEeEWiFfpR_IwGFip4R54dbVk',
+    authDomain: 'territorios-422c2.firebaseapp.com',
+    messagingSenderId: '1086542552314',
+  },
   production: false
 };
 
