5112c2c512e8c00a91fdce222ce9dabf3cb2c5d4
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-05-18 13:05:58 -0300

    feat: finish add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SpinnerService } from '@core/services/spinner.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   isLoading$: boolean = false;
 
   constructor(private spinner: SpinnerService) {}
