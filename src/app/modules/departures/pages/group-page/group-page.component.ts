77e77f10d6c1459bf51a06fac0b7580d6b82d44b
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-05-23 18:12:00 -0300

    feat: list departures and delete modal form in group departures

--- src/app/modules/departures/pages/group-page/group-page.component.ts
+++ src/app/modules/departures/pages/group-page/group-page.component.ts
@@ -12,12 +12,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class GroupPageComponent implements OnInit {
   routerBreadcrum: any = [];
-  isAdmin: boolean = false;
   numberGroup: any;
   dateDeparture: any = new FormControl("");
   departures$: any = [];
-  editDepartures: any = {};
-  formDeparture: FormGroup;
   constructor(
     private routerBreadcrumMockService: RouterBreadcrumMockService,
     private territoryDataService: TerritoryDataService,
@@ -27,33 +24,14 @@ export class GroupPageComponent implements OnInit {
   ){
     this.spinner.cargarSpinner();
     this.routerBreadcrum = routerBreadcrumMockService.getBreadcrum();
-    if(localStorage.getItem("tokenAdmin")){
-      this.isAdmin = true;
-    }
-    this.formDeparture = this.fb.group({
-      departure: new FormArray([])
-    });
     this.numberGroup = this.rutaActiva.snapshot.params;
   }
   ngOnInit(): void {
     this.routerBreadcrum = this.routerBreadcrum[11];
-    this.territoryDataService.getDepartures(this.numberGroup.number).subscribe({
+    this.territoryDataService.getDepartures().subscribe({
       next: (departure) => {
         // Tabla de salidas
-        this.departures$ = departure.departure;                
-        // Datos para editar        
-        this.editDepartures = JSON.parse(JSON.stringify(departure));
-        this.departureFormArray.clear()        
-        this.editDepartures.departure.map((departure: any, index: number) => {
-          this.departureFormArray.push(this.fb.group({
-            day: new FormControl(departure.day),
-            driver: new FormControl(departure.driver),
-            schedule: new FormControl(departure.schedule),
-            territory: new FormControl(departure.territory),
-            point: new FormControl(departure.point),
-            checked: new FormControl(departure.checked),
-          }));
-        });
+        this.departures$ = departure.departure;
         this.territoryDataService.getDateDepartures().subscribe({
           next: (date) => {
             this.dateDeparture.setValue(date.date);
@@ -62,54 +40,5 @@ export class GroupPageComponent implements OnInit {
         });
       }
     })
-  }
-  onChangeInput(e:any, key: any, indexChange: any){
-    this.departureFormArray.controls.forEach((item: any, indexArray: any) => {
-      if(key === item.value.day && indexChange === indexArray){   
-        item.value.day = e.target.value;       
-      } else if(key === item.value.driver && indexChange === indexArray){
-        item.value.driver = e.target.value;     
-      } else if(key === item.value.schedule && indexChange === indexArray){
-        item.value.schedule = e.target.value; 
-      } else if(key === item.value.territory && indexChange === indexArray){
-        item.value.territory = e.target.value; 
-      } else if(key === item.value.point && indexChange === indexArray){
-        item.value.point = e.target.value; 
-      } else if(key === item.value.checked && indexChange === indexArray){
-        item.value.checked = e.target.checked;
-      }
-    })
-  }
-  get departureFormArray() {
-    return this.formDeparture.get('departure') as FormArray;
-  }
-  addInputForm(){
-    this.departureFormArray.push(this.fb.group({
-      day: new FormControl(''),
-      driver: new FormControl(''),
-      schedule: new FormControl(''),
-      territory: new FormControl(''),
-      point: new FormControl(''),
-    }));
-  }
-  deleteInputForm(index: number){  
-    this.departureFormArray.removeAt(index)
-  }
-  rollbackInputForm(){
-    this.departureFormArray.clear()
-    this.editDepartures.departure.map((departure: any, index: number) => {          
-      this.departureFormArray.push(this.fb.group({
-        day: new FormControl(departure.day),
-        driver: new FormControl(departure.driver),
-        schedule: new FormControl(departure.schedule),
-        territory: new FormControl(departure.territory),
-        point: new FormControl(departure.point),
-        checked: new FormControl(departure.checked),
-      }));
-    });
-  }
-  submitForm(){
-    this.territoryDataService.putDate({date: this.dateDeparture.value});
-    this.territoryDataService.putDepartures(this.formDeparture.value, this.numberGroup.number);
   }
 }
