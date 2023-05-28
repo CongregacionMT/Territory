6b3caf910afadf39dd9961d9815ef22e8508213d
JoniMeichtry11 <joni.meichtry@gmail.com>
2023-05-28 14:44:57 -0300

    fix: editar salidas show if tokenAdmin is true

--- src/app/modules/departures/pages/home-departure-page/home-departure-page.component.ts
+++ src/app/modules/departures/pages/home-departure-page/home-departure-page.component.ts
@@ -22,6 +22,7 @@ export class HomeDeparturePageComponent implements OnInit{
     private router: Router,
   ) {
     this.routerBreadcrum = routerBreadcrumMockService.getBreadcrum();
+    localStorage.getItem('tokenAdmin') ? this.isAdmin = true : this.isAdmin = false;
   }
   ngOnInit(): void {
     this.spinner.cargarSpinner();
