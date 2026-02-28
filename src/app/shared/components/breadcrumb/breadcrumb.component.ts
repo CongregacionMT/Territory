983a36fb6a93b67a428ccf7c60030ad77c51ddbd
JoniMeichtry11 <joni.meichtry@gmail.com>
2026-02-28 15:09:25 -0300

    fix: refactor router bradcrum

--- src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { BreadcrumbItem } from '@core/models/Breadcrumb';
 
 @Component({
-    selector: 'app-breadcrumb',
-    templateUrl: './breadcrumb.component.html',
-    styleUrls: ['./breadcrumb.component.scss'],
-    imports: [RouterLink]
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss'],
+  imports: [RouterLink],
 })
-export class BreadcrumbComponent implements OnInit {
-  readonly routerBreadcrum = input<BreadcrumbItem[]>();
-  returnBack: string = "../";
-  ultimateElement: string = "";
-  constructor() {}
+export class BreadcrumbComponent {
+  readonly routerBreadcrum = input<BreadcrumbItem[]>([]);
+  readonly returnBack = '../';
 
-  ngOnInit(): void {
-    const breadcrumArr = this.routerBreadcrum();
-    if (breadcrumArr && breadcrumArr.length > 0) {
-      this.ultimateElement = breadcrumArr[breadcrumArr.length - 1].route;
-      breadcrumArr.pop();
-    }
-  }
+  readonly breadcrumbItems = computed(() => {
+    const list = this.routerBreadcrum() || [];
+    return list.slice(0, -1);
+  });
+
+  readonly ultimateElement = computed(() => {
+    const list = this.routerBreadcrum() || [];
+    return list.length > 0 ? list[list.length - 1].route : '';
+  });
 }
